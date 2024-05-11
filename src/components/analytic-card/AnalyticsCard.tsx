81a03749f8a1fa357bc98976cfa63bf35e4ed054
Sebastian Anioke <aniokechukwudi7@gmail.com>
2024-05-11 11:09:16 +0100

    analytics

--- src/components/analytic-card/AnalyticsCard.tsx
+++ src/components/analytic-card/AnalyticsCard.tsx
@@ -1,5 +1,6 @@
+import { cn } from "../../utils/cn";
 import { getPercent } from "../../utils/getPercent";
-
+import clsx from "clsx";
 type AnalyticsCardProp = {
    title: string;
    total: number 
@@ -11,20 +12,21 @@ type AnalyticsCardProp = {
 }
 
 function AnalyticsCard({title, total, percent, icon, filter, color}: AnalyticsCardProp) {
-   const calculatedPercent = getPercent(total, percent)
+   const calculatedPercent = getPercent(total, percent);
+   const widthPecent = clsx(`w-[${Math.floor(Number(calculatedPercent))}%]`);
    
   return (
     <article className="w-full p-4 border border-gray-300-400 rounded-lg">
       <div className="flex justify-between items-center mb-4">
          <h1 className="text-2xl font-bold">{total}</h1>
-         <span className="w-7 h-7 rounded-full flex justify-center items-center ">
+         <span className={`w-7 h-7 rounded-full ${color} flex justify-center items-center `}>
             <img className={`${filter} w-full`} src={icon} alt={icon} />
          </span>
       </div>
       <p className="text-sm text-gray-500">{title}</p>
       <div className="flex justify-between items-center gap-3 mt-3">
-         <div className={` h-1 rounded-md ${color} w-full bg-gray-200 flex items-center `}>
-            <span className={`w-[${calculatedPercent}%] block h-full ${color}`} />
+         <div className={` h-1 rounded-md  w-full bg-gray-200 flex items-center relative`}>
+            <span style={{width: `${calculatedPercent}%`}} className={` block h-full ${color} absolute`} />
          </div>
          <p className="text-sm text-gray-400">{calculatedPercent}%</p>
       </div>
--- src/components/dashboard/side-bar/nav-bar/NavBar.tsx
+++ src/components/dashboard/side-bar/nav-bar/NavBar.tsx
@@ -3,7 +3,7 @@ function NavBar() {
 		<nav className="row-span-1 col-span-full flex justify-between items-center px-8 shadow-md  fixed w-full bg-white h-[10vh] z-20">
 			<div className="flex items-center gap-2">
 				<img className="w-7 " src="/icons/logo.svg" alt="logo" />
-				<h2 className="font-medium">studyIQ</h2>
+				<h2 className="font-medium uppercase">studyIQ</h2>
 			</div>
          <h1 className="font-bold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">Dashboard</h1>
          <span className="w-7 h-7 p-1 rounded-full flex justify-center items-center">
