5155181a4ab2e8b554ab3b65f87205cc7fec9fb1
Sebastian Anioke <aniokechukwudi7@gmail.com>
2024-05-11 02:25:37 +0100

    made new changes

--- src/pages/quiz-page/QuizBoard.tsx
+++ src/pages/quiz-page/QuizBoard.tsx
@@ -1,6 +1,6 @@
 function Top() {
 	return (
-		<nav className="row-span-1 col-span-full flex justify-between items-center px-8 shadow-md  w-full bg-white h-[10vh] z-20">
+		<nav className="row-span-1 col-span-full flex justify-between items-center px-8 shadow-md  w-full bg-white h-[10vh] z-20 relative">
 			<div className="flex items-center gap-2">
 				<img className="w-7 " src="/icons/logo.svg" alt="logo" />
 				<h2 className="font-medium">studyIQ</h2>
@@ -17,8 +17,12 @@ function Top() {
 
 function QuizBoard() {
   return (
-    <div className="fixed z-50 top-0 left-0 w-full">
+    <div className="fixed z-50 top-0 left-0 w-full h-screen bg-white">
       <Top />
+      <div className="mt-6">
+         <h2 className="text-center uppercase font-bold text-gray-400">QUESTION 1/3</h2>
+         <p className="text-center mt-6 font-semibold capitalize text-lg w-[50%] mx-auto">Have you praciced sports or any physical activity outside your working hours?</p>
+      </div>
 
     </div>
   )
